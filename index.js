b227cb07aaa35d041cc921a52d3e69a6d72217b1
Sofinaye <abene.fekadu@gmail.com>
2023-04-27 23:34:21 +0300

    Add object of teachers data

--- index.js
+++ index.js
@@ -1,8 +1,72 @@
+const teachers = {
+  title: 'SehYeong Noh',
+  description: 'Director of Frontend department and member of University board.',
+  descriptionn: 'SehYeong Noh has attended his Masters and Phd degree in this university and He was amongest the best achiveres and he is currently Teaching in this university.',
+  img: './img/teach.png'
+}
+
 const menuIcon = document.querySelector('#menu-icon');
 const menuToggle = document.querySelector('#menu-toggle');
 const menuSection = document.querySelector('#menu-section');
 
-menuIcon.addEventListener('click', () => {
+{/* <h2 class="teacher-heading">Out Teachers</h2>
+    <div class="teacher-under"></div>
+    <div class="teachers-container">
+      <div class="teacher1">
+        <div class="teacher-body">
+          <div class="teacher-img-container"><img src="./img/teach.png" alt="teacher" class="teacher-img"></div>
+          <div class="teacher-disc-body">
+          <h2 class="teacher-title">SohYeong Noh</h2>
+          <p class="teacher-disc">Director of Frontend department and member of University board.</p>
+          <p class="teacher-disc2">SehYeong Noh has attended his Masters and Phd degree in this university and He was amongest the best achiveres and he is currently Teaching in this university.</p>
+        </div>
+        </div>
+        <div class="teacher-body">
+          <div class="teacher-img-container"><img src="./img/teach.png" alt="teacher" class="teacher-img"></div>
+          <div class="teacher-disc-body">
+          <h2 class="teacher-title">SohYeong Noh</h2>
+          <p class="teacher-disc">Director of Frontend department and member of University board.</p>
+          <p class="teacher-disc2">SehYeong Noh has attended his Masters and Phd degree in this university and He was amongest the best achiveres and he is currently Teaching in this university.</p>
+        </div>
+        </div>
+        <div class="teacher-body">
+          <div class="teacher-img-container"><img src="./img/teach.png" alt="teacher" class="teacher-img"></div>
+          <div class="teacher-disc-body">
+          <h2 class="teacher-title">SohYeong Noh</h2>
+          <p class="teacher-disc">Director of Frontend department and member of University board.</p>
+          <p class="teacher-disc2">SehYeong Noh has attended his Masters and Phd degree in this university and He was amongest the best achiveres and he is currently Teaching in this university.</p>
+        </div>
+        </div>
+      </div>
+      <div class="teacher2">
+        <div class="teacher-body">
+          <div class="teacher-img-container"><img src="./img/teach.png" alt="teacher" class="teacher-img"></div>
+          <div class="teacher-disc-body">
+          <h2 class="teacher-title">SohYeong Noh</h2>
+          <p class="teacher-disc">Director of Frontend department and member of University board.</p>
+          <p class="teacher-disc2">SehYeong Noh has attended his Masters and Phd degree in this university and He was amongest the best achiveres and he is currently Teaching in this university.</p>
+        </div>
+        </div>
+        <div class="teacher-body">
+          <div class="teacher-img-container"><img src="./img/teach.png" alt="teacher" class="teacher-img"></div>
+          <div class="teacher-disc-body">
+          <h2 class="teacher-title">SohYeong Noh</h2>
+          <p class="teacher-disc">Director of Frontend department and member of University board.</p>
+          <p class="teacher-disc2">SehYeong Noh has attended his Masters and Phd degree in this university and He was amongest the best achiveres and he is currently Teaching in this university.</p>
+        </div>
+        </div>
+        <div class="teacher-body">
+          <div class="teacher-img-container"><img src="./img/teach.png" alt="teacher" class="teacher-img"></div>
+          <div class="teacher-disc-body">
+          <h2 class="teacher-title">SohYeong Noh</h2>
+          <p class="teacher-disc">Director of Frontend department and member of University board.</p>
+          <p class="teacher-disc2">SehYeong Noh has attended his Masters and Phd degree in this university and He was amongest the best achiveres and he is currently Teaching in this university.</p>
+        </div>
+        </div>
+      </div>
+    </div> */}
+
+  menuIcon.addEventListener('click', () => {
   menuSection.classList.add('menu-section-active');
 });
 
